589af8793e7304eb4311ecc782f33098f5e4e4df
Elvin Endrenal <endrenalelvin@gmail.com>
2024-10-07 22:01:19 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={
           <>
-            <Navbar />
+            <Navbar />  
             <HomeSection />
             <AboutSection />
             <NumbersSections />
