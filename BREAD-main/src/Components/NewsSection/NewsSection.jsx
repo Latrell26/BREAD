728d5946afc3a90c5cff6d8c7b0f1da99a6eed25
matthew-dev-21 <manalangmatthew2@gmail.com>
2024-10-25 22:57:21 +0800

    upt

---
+++ BREAD-main/src/Components/NewsSection/NewsSection.jsx
@@ -0,0 +1,22 @@
+// This is the news section
+import React from 'react';
+import './NewsSection.css'
+
+const NewsSection = () => {
+  return (
+    <div className="fifth-layer">
+      <section id="news" className="news-section">
+        <h1>Bread News</h1>
+        <p>Stay updated with the latest from Bread. Read about our recent donations, community stories, and more!</p>
+        <main className="news-box">
+          <div className="news-box-1"></div>
+          <div className="news-box-1"></div>
+          <div className="news-box-1"></div>
+          <div className="news-box-1"></div>
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default NewsSection;
